6d5e6734dbb781ab5627501ddcd65a0068131a43
S1rCoDy <148563516+S1rCoDy@users.noreply.github.com>
2024-01-09 17:30:42 +0500

    + localstorage

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,23 +1,39 @@
-import React,{useState,useRef} from "react";
+import React, { useState, useEffect } from "react";
 
 const Content = () => {
-	
+  const [text, setText] = useState('');
 
-	return(
-		<main className="MainContent">
-			<div className="Title">
-				<h1>Deary</h1>
-			</div>
-			<div className="TEXT">
-				<textarea  cols="30" rows="10"></textarea>
-			</div>
-			<div className="Save">
-				<div className="SaveButton">
-					<button>Save</button>
-				</div>
-			</div>
-		</main>
-	)
+  useEffect(() => {
+    const savedText = localStorage.getItem('myData');
+    if (savedText) {
+      setText(savedText);
+    }
+  }, []);
+
+  const handleSave = () => {
+    localStorage.setItem('myData', text);
+  };
+
+  return (
+    <main className="MainContent">
+      <div className="Title">
+        <h1>Deary</h1>
+      </div>
+      <div className="TEXT">
+        <textarea 
+          cols="30" 
+          rows="10"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+      </div>
+      <div className="Save">
+        <div className="SaveButton">
+          <button onClick={handleSave}>Save</button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Content;
