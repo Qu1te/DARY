3b5724a1eb5cdb3b21584b267fb88c41124592d4
S1rCoDy <148563516+S1rCoDy@users.noreply.github.com>
2024-01-10 14:49:45 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Main from "./pages/Main";
+import AboutUs from "./components/About";
+import { getAllByLabelText } from "@testing-library/react";
 import {BrowserRouter,RouterProvider,createBrowserRouter} from "react-router-dom"
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element:<Main/>
+    },
+    {
+      path: "/about",
+      element:<AboutUs/>
     },
     
   ])
