7327e6e2b1290d76624dbaf529bc622d830b896d
S1rCoDy <148563516+S1rCoDy@users.noreply.github.com>
2024-01-10 14:48:20 +0500

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import "../pages/About.css"
+
+const AboutUs = () => {
+    return(
+    <div>
+        <Header/>
+        <div>
+            <h1 className="name">About of Deary</h1>
+        </div>
+    </div>
+    );
+}
+
+export default AboutUs;
